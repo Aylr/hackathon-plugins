3dc268e72ee49f116c23d7116670cea2e58e2986
Rex Boyce <rex@superconductive.com>
2020-07-15 13:19:24 -0600

    script for running validations

---
+++ dickens_trends_script.py
@@ -0,0 +1,14 @@
+import great_expectations
+from glob import glob
+
+context = great_expectations.DataContext()
+
+# TODO loading batches will move into DataContext eventually
+batches_to_validate = []
+for file in glob('annual/*.csv'):
+    batch_kwargs = {'datasource': 'annual__dir', 'path': file}
+    batch = context.get_batch(batch_kwargs, 'dickens_trends')
+    results = context.run_validation_operator(
+        'action_list_operator',
+        assets_to_validate=[batch],
+    )
